ab4db67283da0b9d662984a4c486465624ef0a94
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-17 20:15:56 +0800

    Update App.tsx

--- examples/vite-example/src/App.tsx
+++ examples/vite-example/src/App.tsx
@@ -2,6 +2,10 @@ export default function App(): JSX.Element {
   signal: var count = 0;
   memo: var message = `Count: ${count}`;
 
+  effect: {
+    console.log(message);
+  }
+
   function increment() {
     count += 1;
   }
