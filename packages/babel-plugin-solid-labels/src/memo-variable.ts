ce99ffc80bcd023988b5b65b908ac0c3662c3a9b
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-24 13:38:47 +0800

    Fix `memo` to support dev mode

--- packages/babel-plugin-solid-labels/src/memo-variable.ts
+++ packages/babel-plugin-solid-labels/src/memo-variable.ts
@@ -1,33 +1,93 @@
 import { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
 import accessorVariableExpression from './accessor-variable';
-import { ImportHook } from './types';
+import { State } from './types';
 
 export default function memoVariableExpression(
-  hooks: ImportHook,
+  state: State,
   path: NodePath<t.VariableDeclarator>,
   memoIdentifier: t.Identifier,
   stateIdentifier: t.Expression,
   optionsIdentifier?: t.Expression,
 ): void {
-  accessorVariableExpression(
-    hooks,
-    path,
-    memoIdentifier,
-    'createMemo',
-    [
-      t.arrowFunctionExpression(
-        [],
-        stateIdentifier,
-      ),
-      t.identifier('undefined'),
-      t.objectExpression([
-        t.objectProperty(
-          t.identifier('name'),
-          t.stringLiteral(memoIdentifier.name),
+  if (state.opts.dev) {
+    if (optionsIdentifier) {
+      accessorVariableExpression(
+        state,
+        path,
+        memoIdentifier,
+        'createMemo',
+        [
+          t.arrowFunctionExpression(
+            [],
+            stateIdentifier,
+          ),
+          t.identifier('undefined'),
+          t.callExpression(
+            t.memberExpression(
+              t.identifier('Object'),
+              t.identifier('assign'),
+            ),
+            [
+              t.objectExpression([
+                t.objectProperty(
+                  t.identifier('name'),
+                  t.stringLiteral(memoIdentifier.name),
+                ),
+              ]),
+              optionsIdentifier,
+            ],
+          ),
+        ],
+      );
+    } else {
+      accessorVariableExpression(
+        state,
+        path,
+        memoIdentifier,
+        'createMemo',
+        [
+          t.arrowFunctionExpression(
+            [],
+            stateIdentifier,
+          ),
+          t.identifier('undefined'),
+          t.objectExpression([
+            t.objectProperty(
+              t.identifier('name'),
+              t.stringLiteral(memoIdentifier.name),
+            ),
+          ]),
+        ],
+      );
+    }
+  } else if (optionsIdentifier) {
+    accessorVariableExpression(
+      state,
+      path,
+      memoIdentifier,
+      'createMemo',
+      [
+        t.arrowFunctionExpression(
+          [],
+          stateIdentifier,
         ),
-        ...(optionsIdentifier ? [t.spreadElement(optionsIdentifier)] : <t.SpreadElement[]>[]),
-      ]),
-    ],
-  );
+        t.identifier('undefined'),
+        optionsIdentifier,
+      ],
+    );
+  } else {
+    accessorVariableExpression(
+      state,
+      path,
+      memoIdentifier,
+      'createMemo',
+      [
+        t.arrowFunctionExpression(
+          [],
+          stateIdentifier,
+        ),
+      ],
+    );
+  }
 }
