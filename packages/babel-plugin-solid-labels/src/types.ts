8f270ec811a3107af2335e04c70939b3db6b8b50
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-24 13:33:50 +0800

    Add `Options`

--- packages/babel-plugin-solid-labels/src/types.ts
+++ packages/babel-plugin-solid-labels/src/types.ts
@@ -1,7 +1,13 @@
+import { PluginPass } from '@babel/core';
 import * as t from '@babel/types';
 
 export type ImportHook = Map<string, t.Identifier>
 
-export interface State {
+export interface Options {
+  dev?: boolean;
+}
+
+export interface State extends PluginPass {
   hooks: ImportHook;
+  opts: Options;
 }
