88511de320af1c4c41bef9b2cd7c13be3a991e2b
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-24 13:35:19 +0800

    Fix `signal` to support dev mode

--- packages/babel-plugin-solid-labels/src/signal-variable.ts
+++ packages/babel-plugin-solid-labels/src/signal-variable.ts
@@ -2,10 +2,10 @@ import { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
 import derefSignalExpression from './deref-signal-expression';
 import getHookIdentifier from './get-hook-identifier';
-import { ImportHook } from './types';
+import { State } from './types';
 
 export default function signalVariableExpression(
-  hooks: ImportHook,
+  state: State,
   path: NodePath<t.VariableDeclarator>,
   signalIdentifier: t.Identifier,
   stateIdentifier: t.Expression = t.identifier('undefined'),
@@ -18,19 +18,59 @@ export default function signalVariableExpression(
     readIdentifier,
     writeIdentifier,
   ]);
-  path.node.init = t.callExpression(
-    getHookIdentifier(hooks, path, 'createSignal'),
-    [
-      stateIdentifier,
-      t.objectExpression([
-        t.objectProperty(
-          t.identifier('name'),
-          t.stringLiteral(signalIdentifier.name),
-        ),
-        ...(optionsIdentifier ? [t.spreadElement(optionsIdentifier)] : <t.SpreadElement[]>[]),
-      ]),
-    ],
-  );
+  if (state.opts.dev) {
+    if (optionsIdentifier) {
+      path.node.init = t.callExpression(
+        getHookIdentifier(state.hooks, path, 'createSignal'),
+        [
+          stateIdentifier,
+          t.callExpression(
+            t.memberExpression(
+              t.identifier('Object'),
+              t.identifier('assign'),
+            ),
+            [
+              t.objectExpression([
+                t.objectProperty(
+                  t.identifier('name'),
+                  t.stringLiteral(signalIdentifier.name),
+                ),
+              ]),
+              optionsIdentifier,
+            ],
+          ),
+        ],
+      );
+    } else {
+      path.node.init = t.callExpression(
+        getHookIdentifier(state.hooks, path, 'createSignal'),
+        [
+          stateIdentifier,
+          t.objectExpression([
+            t.objectProperty(
+              t.identifier('name'),
+              t.stringLiteral(signalIdentifier.name),
+            ),
+          ]),
+        ],
+      );
+    }
+  } else if (optionsIdentifier) {
+    path.node.init = t.callExpression(
+      getHookIdentifier(state.hooks, path, 'createSignal'),
+      [
+        stateIdentifier,
+        optionsIdentifier,
+      ],
+    );
+  } else {
+    path.node.init = t.callExpression(
+      getHookIdentifier(state.hooks, path, 'createSignal'),
+      [
+        stateIdentifier,
+      ],
+    );
+  }
 
   derefSignalExpression(
     path,
