741a390fb82c9b2ea2fafa0400e4b5ba00db7613
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-09-24 13:41:05 +0800

    Fix `deferred` to support dev mode

--- packages/babel-plugin-solid-labels/src/deferred-variable.ts
+++ packages/babel-plugin-solid-labels/src/deferred-variable.ts
@@ -1,32 +1,90 @@
 import { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
 import accessorVariableExpression from './accessor-variable';
-import { ImportHook } from './types';
+import { State } from './types';
 
 export default function deferredVariableExpression(
-  hooks: ImportHook,
+  state: State,
   path: NodePath<t.VariableDeclarator>,
   deferredIdentifier: t.Identifier,
   stateIdentifier: t.Expression = t.identifier('undefined'),
   optionsIdentifier?: t.Expression,
 ): void {
-  accessorVariableExpression(
-    hooks,
-    path,
-    deferredIdentifier,
-    'createDeferred',
-    [
-      t.arrowFunctionExpression(
-        [],
-        stateIdentifier,
-      ),
-      t.objectExpression([
-        t.objectProperty(
-          t.identifier('name'),
-          t.stringLiteral(deferredIdentifier.name),
+  if (state.opts.dev) {
+    if (optionsIdentifier) {
+      accessorVariableExpression(
+        state,
+        path,
+        deferredIdentifier,
+        'createDeferred',
+        [
+          t.arrowFunctionExpression(
+            [],
+            stateIdentifier,
+          ),
+          t.callExpression(
+            t.memberExpression(
+              t.identifier('Object'),
+              t.identifier('assign'),
+            ),
+            [
+              t.objectExpression([
+                t.objectProperty(
+                  t.identifier('name'),
+                  t.stringLiteral(deferredIdentifier.name),
+                ),
+              ]),
+              optionsIdentifier,
+            ],
+          ),
+        ],
+      );
+    } else {
+      accessorVariableExpression(
+        state,
+        path,
+        deferredIdentifier,
+        'createDeferred',
+        [
+          t.arrowFunctionExpression(
+            [],
+            stateIdentifier,
+          ),
+          t.objectExpression([
+            t.objectProperty(
+              t.identifier('name'),
+              t.stringLiteral(deferredIdentifier.name),
+            ),
+          ]),
+        ],
+      );
+    }
+  } else if (optionsIdentifier) {
+    accessorVariableExpression(
+      state,
+      path,
+      deferredIdentifier,
+      'createDeferred',
+      [
+        t.arrowFunctionExpression(
+          [],
+          stateIdentifier,
         ),
-        ...(optionsIdentifier ? [t.spreadElement(optionsIdentifier)] : <t.SpreadElement[]>[]),
-      ]),
-    ],
-  );
+        optionsIdentifier,
+      ],
+    );
+  } else {
+    accessorVariableExpression(
+      state,
+      path,
+      deferredIdentifier,
+      'createDeferred',
+      [
+        t.arrowFunctionExpression(
+          [],
+          stateIdentifier,
+        ),
+      ],
+    );
+  }
 }
