a3af001a99dfb7edc49446b2cc9ea73f14947da7
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-01-31 18:29:03 +0800

    Fix `normalizeAccessor`

--- packages/babel-plugin-solid-labels/src/normalize-accessor.ts
+++ packages/babel-plugin-solid-labels/src/normalize-accessor.ts
@@ -3,14 +3,16 @@ import * as t from '@babel/types';
 export default function normalizeAccessor(
   identifier: t.Expression,
 ): t.Expression {
-  return (
-    t.isCallExpression(identifier)
-    && identifier.arguments.length === 0
-    && t.isExpression(identifier.callee)
-      ? identifier.callee
-      : t.arrowFunctionExpression(
-        [],
-        identifier,
-      )
-  );
+  // TODO Fix optimizer for call expressions
+  // return (
+  //   t.isCallExpression(identifier)
+  //   && identifier.arguments.length === 0
+  //   && t.isExpression(identifier.callee)
+  //     ? identifier.callee
+  //     : t.arrowFunctionExpression(
+  //       [],
+  //       identifier,
+  //     )
+  // );
+  return t.arrowFunctionExpression([], identifier);
 }
