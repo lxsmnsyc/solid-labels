fa60af92821128f2b4a929b3f5a42f1716517471
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-02-18 21:42:25 +0800

    Fix configs

--- examples/ctf/vite.config.js
+++ examples/ctf/vite.config.js
@@ -5,6 +5,11 @@ import solidLabels from 'vite-plugin-solid-labels';
 export default defineConfig({
   plugins: [
     solidPlugin({}),
-    solidLabels({}),
+    solidLabels({
+      filter: {
+        include: 'src/**/*.{ts,js,tsx,jsx}',
+        exclude: 'node_modules/**/*.{ts,js,tsx,jsx}',
+      },
+    }),
   ],
 });
