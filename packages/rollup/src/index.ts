c7913708a868b0ab2cbd5308e87df0dcf613ecbc
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-19 16:26:07 +0800

    Fix source maps

--- packages/rollup/src/index.ts
+++ packages/rollup/src/index.ts
@@ -3,7 +3,6 @@ import { Plugin } from 'rollup';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import * as babel from '@babel/core';
 import path from 'path';
-import ts from '@babel/preset-typescript';
 
 export interface SolidLabelsPluginFilter {
   include?: FilterPattern;
@@ -32,25 +31,38 @@ export default function solidLabelsPlugin(
     name: 'solid-labels',
     async transform(code, id) {
       if (filter(id)) {
+        const pluginOption = [solidLabelsBabel, {
+          disabled: options.disabled,
+          dev: options.dev,
+        }];
+        const plugins: NonNullable<NonNullable<babel.TransformOptions['parserOpts']>['plugins']> = ['jsx'];
+        if (/\.[mc]?tsx?$/i.test(id)) {
+          plugins.push('typescript');
+        }
         const result = await babel.transformAsync(code, {
           ...options.babel,
-          presets: [
-            [ts],
-            ...(options.babel?.presets ?? []),
-          ],
           plugins: [
-            [solidLabelsBabel, {
-              dev: options.dev,
-              disabled: options.disabled,
-            }],
-            ...(options.babel?.plugins ?? []),
+            pluginOption,
+            ...(options.babel?.plugins || []),
           ],
+          parserOpts: {
+            ...(options.babel?.parserOpts || {}),
+            plugins: [
+              ...(options.babel?.parserOpts?.plugins || []),
+              ...plugins,
+            ],
+          },
           filename: path.basename(id),
+          ast: false,
+          sourceMaps: true,
+          configFile: false,
+          babelrc: false,
+          sourceFileName: id,
         });
 
         if (result) {
           return {
-            code: result.code ?? '',
+            code: result.code || '',
             map: result.map,
           };
         }
--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -3,7 +3,6 @@ import { Plugin } from 'vite';
 import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import * as babel from '@babel/core';
 import path from 'path';
-import ts from '@babel/preset-typescript';
 
 export interface SolidLabelsPluginFilter {
   include?: FilterPattern;
@@ -65,25 +64,38 @@ export default function solidLabelsPlugin(
     },
     async transform(code, id) {
       if (filter(id)) {
+        const pluginOption = [solidLabelsBabel, {
+          disabled: options.disabled,
+          dev: isDev,
+        }];
+        const plugins: NonNullable<NonNullable<babel.TransformOptions['parserOpts']>['plugins']> = ['jsx'];
+        if (/\.[mc]?tsx?$/i.test(id)) {
+          plugins.push('typescript');
+        }
         const result = await babel.transformAsync(code, {
           ...options.babel,
-          presets: [
-            [ts],
-            ...(options.babel?.presets ?? []),
-          ],
           plugins: [
-            [solidLabelsBabel, {
-              disabled: options.disabled,
-              dev: isDev,
-            }],
-            ...(options.babel?.plugins ?? []),
+            pluginOption,
+            ...(options.babel?.plugins || []),
           ],
+          parserOpts: {
+            ...(options.babel?.parserOpts || {}),
+            plugins: [
+              ...(options.babel?.parserOpts?.plugins || []),
+              ...plugins,
+            ],
+          },
           filename: path.basename(id),
+          ast: false,
+          sourceMaps: true,
+          configFile: false,
+          babelrc: false,
+          sourceFileName: id,
         });
 
         if (result) {
           return {
-            code: result.code ?? '',
+            code: result.code || '',
             map: result.map,
           };
         }
